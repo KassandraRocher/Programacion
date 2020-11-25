75ff1caf31059fce77659686e3e17cf48145434f
Kassandra Rocher <74734063+KassandraRocher@users.noreply.github.com>
2020-11-25 20:00:22 +0100

    Add files via upload

---
+++ UD 3. Kassandra Rocher/UD3_Ejercicio1.java
@@ -0,0 +1,15 @@
+public class UD3_Ejercicio1 {
+    public static void main (String[] args){
+        System.out.println ("Table \t Mesa");
+        System.out.println ("Chair \t Silla");
+        System.out.println ("Pencil \t Lápiz");
+        System.out.println ("Apple \t Manzana");
+        System.out.println ("Flower \t Flor");
+        System.out.println ("Floor \t Suelo");
+        System.out.println ("Pen \t Boligrafo");
+        System.out.println ("Book \t Libro");
+        System.out.println ("Car \t Coche");
+        System.out.println ("Truck \t Camión");
+
+    }
+}
---
+++ UD 3. Kassandra Rocher/UD3_Ejercicio3.java
@@ -0,0 +1,19 @@
+
+public class UD3_Ejercicio3 {
+    public static void main (String[] args){
+    int a = 8;
+    int b = 3;
+    double resultado = (double) a/b;
+
+    System.out.printf ("%1.16f %n", resultado);
+    System.out.printf ("%.2f %n", resultado);
+    System.out.printf("%5.2f %n", resultado);
+    System.out.printf("%7.3f %n", resultado);
+    System.out.printf ("%07.3f %n", resultado);
+    System.out.printf("%10.4f %n", resultado);
+    System.out.printf ("%5.3f %n", resultado);
+    System.out.printf ("%10.5f %n", resultado);
+    System.out.printf("%010.0f %n", resultado);
+    }
+
+}
